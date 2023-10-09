4dfd68ab01a08a38cce57647783f31f408193e8a
stevedev0pensoft <stevedev0pensoft@gmail.com>
2023-10-09 11:53:40 +0300

    meta

--- app/main.py
+++ app/main.py
@@ -3,7 +3,18 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.handlers import router
 
 def get_application() -> FastAPI:
-    application = FastAPI()
+    description="""
+        It is Google maps, but for International Criminal Court 🚀
+        """
+    application = FastAPI(
+        title="Justice.eye",
+        description=description,
+        version="0.2.3",
+        contact={
+            "name": "Figma design",
+            "url": "https://www.figma.com/file/JB4CQHcaj4C7fjNuCQBqWJ/Ukraine-map-etc?type=design&mode=design&t=P3PC8lDzdnppsHyt-1"
+        },
+    )
     application.include_router(router)
     return application
 
